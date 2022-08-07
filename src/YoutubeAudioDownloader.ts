ca1687dd08fe8c6d0af090764d8cef003b14cc6c
ssogon <ssogon95@gmail.com>
2022-08-07 11:48:52 +0900

    fix: windows에서 파일명으로 사용할 수 없는 문자들을 escape 처리

--- src/YoutubeAudioDownloader.ts
+++ src/YoutubeAudioDownloader.ts
@@ -3,20 +3,33 @@ import { YoutubeMetadata } from './YoutubeMetadata';
 
 export class YoutubeAudioDownloader {
   static async download(url: string, youtubeMetadata?: YoutubeMetadata) {
-    const fileName = youtubeMetadata
-      ? `./output/${youtubeMetadata.artist} - ${youtubeMetadata.title}.%(ext)s`
-      : './output/%(title)s.%(ext)s';
+    const fileName = escapeFileName(
+      youtubeMetadata ? `${youtubeMetadata.artist} - ${youtubeMetadata.title}.%(ext)s` : '%(title)s.%(ext)s'
+    );
 
     youtubeDlExec(url, {
       format: 'bestaudio[ext=mp3]/bestaudio[ext=mp4]/bestaudio[ext=m4a]/bestaudio[ext=ogg]',
-      output: fileName,
+      output: `./output/${fileName}`,
       embedThumbnail: true,
       ...(youtubeMetadata && {
-        postprocessorArgs: `-metadata artist=${youtubeMetadata.artist?.replace(
+        postprocessorArgs: `-metadata artist=${youtubeMetadata.artist.replace(
           / /g,
           '\\ '
-        )} -metadata title=${youtubeMetadata.title?.replace(/ /g, '\\ ')}`,
+        )} -metadata title=${youtubeMetadata.title.replace(/ /g, '\\ ')}`,
       }),
     }).then((output) => console.log(output + '\n'));
   }
+}
+
+function escapeFileName(fileName: string) {
+  return fileName
+    .replace(/\\/g, '＼')
+    .replace(/\//g, '／')
+    .replace(/\:/g, '：')
+    .replace(/\*/g, '＊')
+    .replace(/\?/g, '？')
+    .replace(/\"/g, '＂')
+    .replace(/\</g, '＜')
+    .replace(/\>/g, '＞')
+    .replace(/\|/g, '｜');
 }
